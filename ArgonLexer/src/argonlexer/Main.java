848d40c3724a94a33ec1d67d9feecae73d3ee730
dyharlan <dyharlan45@gmail.com>
2024-02-16 00:13:28 +0800

    Initial commit of the hand built lexer

---
+++ ArgonLexer/src/argonlexer/Main.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package argonlexer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ * @author dyhar
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String[] args) {
+        // TODO code application logic here
+        Lexer lex = new Lexer();
+        if (args.length > 0) {
+            try {
+                lex.Analyze(new File(args[0]));
+                
+            } catch (FileNotFoundException fnfe) {
+                System.out.println("File not found.");
+            } catch (IOException ioe) {
+                System.out.println(ioe.getMessage());
+            }catch (IllegalStateException ise) {
+                System.out.println(ise.getMessage());
+            }
+            lex.listTokens();
+        }else{
+            System.out.println("Usage: ArgonLexer.jar [file]");
+        }
+    }
+    
+}
